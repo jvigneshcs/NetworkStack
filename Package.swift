769091df8e041bb43ddefd280973ac49d85b4cb4
Vignesh J <vignesh.jeyaraj2@cognizant.com>
2020-08-13 18:02:46 +0530

    Swift Package configurations

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 let package = Package(
     name: "NetworkStack",
     platforms: [
+        .macOS(.v10_11),
         .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v3)
     ],
     products: [
         .library(
@@ -14,7 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "NetworkStack",
-            path: "NetworkStack"          
+            path: "NetworkStack/Playground/NetworkStack.playground/Sources"          
             ),
     ],
     swiftLanguageVersions: [.v4_2]
